171e445e6f27309a3f262721cfaedbf0ca062368
unknown <amydworsky15@gmail.com>
2022-05-06 15:16:18 -0600

    refactored, created a function to run the selection and user preference functions and slightly edited the user function preference.

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -12,50 +12,37 @@ def randomly_select_from_lists (list):
 #user preference function
 def check_if_user_likes_choice (selected_variable, list_name, list_type):
     user_likes_option = input (f"The {list_type} selected for you is {selected_variable}. Does this sound good? Enter y/n: ")
-    while user_likes_option == "n":
+    while user_likes_option == "n" or user_likes_option == "no":
         selected_variable = randomly_select_from_lists (list_name)
         user_likes_option = input (f"The {list_type} selected for you is {selected_variable}. Does this sound good? Enter y/n: ")
     return selected_variable
 
+#function to run selection and user preference functions
+def selection_and_confirmation (current_list):
+    selection = randomly_select_from_lists (current_list)
+    selection = check_if_user_likes_choice (selection, current_list, "destination")
+    return selection
+
 #destination
-selected_destination = randomly_select_from_lists (destinations)
-selected_destination = check_if_user_likes_choice (selected_destination, destinations, "destination")
+selected_destination = selection_and_confirmation (destinations)
 
 #transportation
-selected_transportation = randomly_select_from_lists (transportation)
-selected_transportation = check_if_user_likes_choice (selected_transportation, transportation, "transportation")
+selected_transportation = selection_and_confirmation (transportation)
 
 #resturant
-selected_resturant = randomly_select_from_lists (resturants)
-selected_resturant = check_if_user_likes_choice (selected_resturant, resturants, "resturant")
+selected_resturant = selection_and_confirmation (resturants)
 
 #entertainment
-selected_entertainment = randomly_select_from_lists (entertainment)
-selected_entertainment = check_if_user_likes_choice (selected_entertainment, entertainment, "entertainment")
-
-print (selected_destination)
-print (selected_transportation)
-print (selected_resturant)
-print (selected_entertainment)
+selected_entertainment = selection_and_confirmation (entertainment)
 
 #final confirmation
-
 final_confirmation = input (f"Congratulations! Your day trip has been generated! You will be taking a {selected_transportation} to {selected_destination}. While there you will {selected_entertainment} and eat at {selected_resturant} resturant for dinner! Does this sound good y/n? ")
 while final_confirmation == "n":
     print ("We're sorry to hear this isn't what you wanted. Lets try again!")
-
-    selected_destination = randomly_select_from_lists (destinations)
-    selected_destination = check_if_user_likes_choice (selected_destination, destinations, "destination")
-
-    selected_transportation = randomly_select_from_lists (transportation)
-    selected_transportation = check_if_user_likes_choice (selected_transportation, transportation, "transportation")
-
-    selected_resturant = randomly_select_from_lists (resturants)
-    selected_resturant = check_if_user_likes_choice (selected_resturant, resturants, "resturant")
-
-    selected_entertainment = randomly_select_from_lists (entertainment)
-    selected_entertainment = check_if_user_likes_choice (selected_entertainment, entertainment, "entertainment")
-
+    selected_destination = selection_and_confirmation (destinations)
+    selected_transportation = selection_and_confirmation (transportation)
+    selected_resturant = selection_and_confirmation (resturants)
+    selected_entertainment = selection_and_confirmation (entertainment)
     final_confirmation = input (f"Congratulations! Your day trip has been generated! You will be taking a {selected_transportation} to {selected_destination}. While there you will {selected_entertainment} and eat at {selected_resturant} resturant for dinner! Does this sound good y/n? ")
 
 print (f"Congratulations! We are so excited that you like your day trip selections! Here is your itinerary: You'll be travelling using a {selected_transportation} to {selected_destination}, while there you'll {selected_entertainment} and eat dinner at {selected_resturant}. Hope you enjoy your time!")
