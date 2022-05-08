88ed563ae35c04fbaeb32ee5e24264f93d427f67
unknown <amydworsky15@gmail.com>
2022-05-08 07:56:51 -0600

    fixed spelling errors

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -1,6 +1,6 @@
 import random
 destinations = ["Lambeau Field", "Levi's Stadium", "Soldier Field", "SoFi Stadium"]
-resturants = ["Taverne in the Sky", "Alinea", "Pho", "Pacific Catch"]
+restaurants = ["Taverne in the Sky", "Alinea", "Pho", "Pacific Catch"]
 transportation = ["boat", "car", "train", "bicycle"]
 entertainment = ["watch a football game", "go on a hot air balloon ride", "go to the beach", "go on a boat tour"]
 
@@ -18,39 +18,39 @@ def check_if_user_likes_choice (selected_variable, list_name, list_type):
     return selected_variable
 
 #function to run selection and user preference functions
-def selection_and_confirmation (current_list):
+def selection_and_confirmation (current_list, list_type):
     selection = randomly_select_from_lists (current_list)
-    selection = check_if_user_likes_choice (selection, current_list, "destination")
+    selection = check_if_user_likes_choice (selection, current_list, list_type)
     return selection
 
 #destination
-selected_destination = selection_and_confirmation (destinations)
+selected_destination = selection_and_confirmation (destinations, "destination")
 
 #transportation
-selected_transportation = selection_and_confirmation (transportation)
+selected_transportation = selection_and_confirmation (transportation, "transportation")
 
 #resturant
-selected_resturant = selection_and_confirmation (resturants)
+selected_restaurant = selection_and_confirmation (restaurants, "restaurant")
 
 #entertainment
-selected_entertainment = selection_and_confirmation (entertainment)
+selected_entertainment = selection_and_confirmation (entertainment, "entertainment")
 
 #final confirmation
-final_confirmation = input (f"Congratulations! Your day trip has been generated! You will be taking a {selected_transportation} to {selected_destination}. While there you will {selected_entertainment} and eat at {selected_resturant} resturant for dinner! Does this sound good y/n? ")
+final_confirmation = input (f"Congratulations! Your day trip has been generated! You will be taking a {selected_transportation} to {selected_destination}. While there you will {selected_entertainment} and eat at {selected_restaurant} restaurant for dinner! Does this sound good y/n? ")
 while final_confirmation == "n":
     print ("We're sorry to hear this isn't what you wanted. Lets try again!")
-    change_selection = input (f"Are you statisfied with {selected_destination}, y/n? ")
+    change_selection = input (f"Are you satisfied with {selected_destination}, y/n? ")
     if change_selection == "n" or change_selection == "no":
-        selected_destination = selection_and_confirmation (destinations)
-    change_selection = input (f"Are you statisfied with {selected_transportation}, y/n? ")
+        selected_destination = selection_and_confirmation (destinations, "destination")
+    change_selection = input (f"Are you satisfied with {selected_transportation}, y/n? ")
     if change_selection == "n" or change_selection == "no":
-        selected_transportation = selection_and_confirmation (transportation)
-    change_selection = input (f"Are you statisfied with {selected_resturant}, y/n? ")
+        selected_transportation = selection_and_confirmation (transportation, "transportation")
+    change_selection = input (f"Are you satisfied with {selected_restaurant}, y/n? ")
     if change_selection == "n" or change_selection == "no":
-        selected_resturant = selection_and_confirmation (resturants)
-    change_selection = input (f"Are you statisfied with {selected_entertainment}, y/n? ")
+        selected_restaurant = selection_and_confirmation (restaurants, "restaurant")
+    change_selection = input (f"Are you satisfied with {selected_entertainment}, y/n? ")
     if change_selection == "n" or change_selection == "no":
-        selected_entertainment = selection_and_confirmation (entertainment)
-    final_confirmation = input (f"Congratulations! Your day trip has been generated! You will be taking a {selected_transportation} to {selected_destination}. While there you will {selected_entertainment} and eat at {selected_resturant} resturant for dinner! Does this sound good y/n? ")
+        selected_entertainment = selection_and_confirmation (entertainment, "entertainment")
+    final_confirmation = input (f"Congratulations! Your day trip has been generated! You will be taking a {selected_transportation} to {selected_destination}. While there you will {selected_entertainment} and eat at {selected_restaurant} restaurant for dinner! Does this sound good y/n? ")
 
-print (f"Congratulations! We are so excited that you like your day trip selections! Here is your itinerary: You'll be travelling using a {selected_transportation} to {selected_destination}, while there you'll {selected_entertainment} and eat dinner at {selected_resturant}. Hope you enjoy your time!")
+print (f"Congratulations! We are so excited that you like your day trip selections! Here is your itinerary: You'll be travelling using a {selected_transportation} to {selected_destination}, while there you'll {selected_entertainment} and eat dinner at {selected_restaurant}. Hope you enjoy your time!")
